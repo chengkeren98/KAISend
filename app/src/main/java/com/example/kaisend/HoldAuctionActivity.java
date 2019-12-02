6c8017b546ee3fd1512f988ceeadc2fcd563a041
37585 <chengkeren98@kaist.ac.kr>
2019-12-02 20:11:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kaisend/HoldAuctionActivity.java
@@ -0,0 +1,14 @@
+package com.example.kaisend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoldAuctionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hold_auction);
+    }
+}
